dca6971d6a8519d9ab6d4683f87fd74cbad46bf3
Ivanko5417 <v.drobysh2011@gmail.com>
2022-02-19 22:34:06 +0300

    feat: copy solution of another student to make sure that it's possible to pass tests

--- solution.go
+++ solution.go
@@ -2,23 +2,31 @@ package square
 
 import "math"
 
-type sidesNumber int
+// Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
+
+// Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
+// it's like:
+// CalcSquare(10.0, SidesTriangle)
+// CalcSquare(10.0, SidesSquare)
+// CalcSquare(10.0, SidesCircle)
+type myInt int
 
 const (
-	SidesCircle   sidesNumber = 0
-	SidesTriangle sidesNumber = 3
-	SidesSquare   sidesNumber = 4
+	SidesCircle   myInt = 0
+	SidesTriangle myInt = 3
+	SidesSquare   myInt = 4
 )
 
-func CalcSquare(sideLen float64, sidesNum sidesNumber) float64 {
-	sideLenSquare := sideLen * sideLen
+func CalcSquare(sideLen float64, sidesNum myInt) float64 {
+	var area float64
+
 	switch sidesNum {
-	case SidesTriangle:
-		return sideLenSquare * math.Sqrt(3) / 4
-	case SidesSquare:
-		return sideLenSquare
 	case SidesCircle:
-		return math.Pi * sideLenSquare
+		area = math.Pi * (sideLen * sideLen)
+	case SidesTriangle:
+		area = (math.Sqrt(3) / 4) * (sideLen * sideLen)
+	case SidesSquare:
+		area = sideLen * sideLen
 	}
-	return 0
+	return area
 }
