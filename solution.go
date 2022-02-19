c0aa2b1028afbf26ec5dd2bff05bd364a3041603
Ivanko5417 <v.drobysh2011@gmail.com>
2022-02-19 22:05:10 +0300

    fix: use capital castom type name

--- solution.go
+++ solution.go
@@ -2,20 +2,13 @@ package square
 
 import "math"
 
-// Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
+type MyCustomInt int
 
-// Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
-// it's like:
-// CalcSquare(10.0, SidesTriangle)
-// CalcSquare(10.0, SidesSquare)
-// CalcSquare(10.0, SidesCircle)
-type myCustomInt int
+const SidesTriangle MyCustomInt = 3
+const SidesSquare MyCustomInt = 4
+const SidesCircle MyCustomInt = 0
 
-const SidesTriangle myCustomInt = 3
-const SidesSquare myCustomInt = 4
-const SidesCircle myCustomInt = 0
-
-func CalcSquare(sideLen float64, sidesNum myCustomInt) float64 {
+func CalcSquare(sideLen float64, sidesNum MyCustomInt) float64 {
 	sideLenSquare := sideLen * sideLen
 	switch sidesNum {
 	case SidesTriangle:
